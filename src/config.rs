c57feba1665d94b1996fbcc4f9f29743d88f62f9
issac <kyrbynyt@gmail.com>
2026-01-14 18:40:44 +0000

    ok we are going going

---
+++ src/config.rs
@@ -0,0 +1,261 @@
+use log::{error, warn};
+pub struct Parser<'a> {
+    input: &'a [Token],
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a [Token]) -> Self {
+        Self { input }
+    }
+
+    pub fn parse(mut self) -> Option<Vec<Statement>> {
+        let mut ss = vec![];
+        while !self.done() {
+            match self.eat().unwrap() {
+                Token::Word(v) if v.as_str() == "Set" => {
+                    let name = match self.eat() {
+                        Some(Token::Word(w)) => w,
+                        Some(other) => {
+                            error!("Unexpected config variable name found {other:?}");
+                            return None;
+                        }
+                        None => {
+                            error!("Expected config variable name");
+                            return None;
+                        }
+                    };
+                    let value = match self.eat() {
+                        Some(Token::Word(w)) => w,
+                        Some(Token::Number(w)) => w.to_string(),
+                        Some(other) => {
+                            error!("Unexpected config value found {other:?}");
+                            return None;
+                        }
+                        None => {
+                            error!("Expected config value");
+                            return None;
+                        }
+                    };
+                    ss.push(Statement::Set {
+                        var: name,
+                        value: value,
+                    });
+                }
+                Token::Word(v) if v.as_str() == "Do" => {
+                    let what = self.parse_action()?;
+                    match self.eat() {
+                        None => {
+                            error!("Missing `on` keyword");
+                            return None;
+                        }
+                        Some(Token::Word(x)) if x.as_str() == "on" => {}
+                        Some(other) => {
+                            error!("Expected `on` keyword found: {other:?}");
+                            return None;
+                        }
+                    }
+                    let combo = self.parse_bind()?;
+                    dbg!(&combo);
+                    ss.push(Statement::Do {
+                        action: what,
+                        on: combo,
+                    });
+                }
+                x => {
+                    warn!("{x:?}");
+                }
+            }
+        }
+        Some(ss)
+    }
+
+    fn parse_action(&mut self) -> Option<Action> {
+        match self.eat() {
+            Some(Token::Word(w)) if w.as_str() == "FocusLeft" => Some(Action::FocusLeft),
+            Some(Token::Word(w)) if w.as_str() == "FocusRight" => Some(Action::FocusRight),
+            Some(other) => {
+                error!("Unexpected config action found {other:?}");
+                return None;
+            }
+            None => {
+                error!("Expected config action name");
+                return None;
+            }
+        }
+    }
+
+    fn parse_bind(&mut self) -> Option<KeyCombo> {
+        let mut prefixes = vec![];
+        let mut has_alt = false;
+        loop {
+            let prefix = self.parse_bind_atom(&mut has_alt)?;
+            if matches!(self.peek(0), Some(Token::Hyphen)) {
+                self.eat(); // consume '-'
+                prefixes.push(prefix);
+            } else {
+                return Some(if prefixes.is_empty() {
+                    prefix
+                } else {
+                    KeyCombo::Prefixed {
+                        prefixes,
+                        key: Box::new(prefix),
+                    }
+                });
+            }
+        }
+    }
+
+    fn parse_bind_atom(&mut self, has_alt: &mut bool) -> Option<KeyCombo> {
+        match self.eat() {
+            None => {
+                return None;
+            }
+            Some(Token::Number(n)) => {
+                return Some(KeyCombo::Char(n.to_string().chars().nth(0).unwrap()));
+            }
+            Some(Token::Word(x)) if x.len() == 1 && x.chars().nth(0).unwrap().is_lowercase() => {
+                return Some(KeyCombo::Char(x.chars().nth(0).unwrap()));
+            }
+            Some(Token::Word(x)) if x.len() == 1 && x.chars().nth(0).unwrap().is_uppercase() => {
+                match x.chars().nth(0).unwrap() {
+                    'M' => Some(KeyCombo::Master),
+                    'A' => Some(KeyCombo::Alt),
+                    'S' => Some(KeyCombo::Space),
+                    'C' => Some(KeyCombo::Control),
+                    'H' => Some(KeyCombo::Shift),
+                    _ => {
+                        todo!()
+                    }
+                }
+            }
+            _ => todo!(),
+        }
+    }
+
+    fn done(&self) -> bool {
+        self.input.is_empty()
+    }
+
+    fn peek(&self, offset: usize) -> Option<&Token> {
+        self.input.get(offset)
+    }
+    fn eat(&mut self) -> Option<Token> {
+        if self.done() {
+            return None;
+        }
+        let t = self.input[0].clone();
+        if self.input.len() >= 1 {
+            self.input = &self.input[1..];
+        }
+        Some(t)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Action {
+    FocusLeft,
+    FocusRight,
+}
+
+#[derive(Debug, Clone)]
+pub enum KeyCombo {
+    Char(char),
+    Alternative(Vec<KeyCombo>),
+    Master,
+    Super,
+    Shift,
+    Alt,
+    Control,
+    Space,
+    Prefixed {
+        prefixes: Vec<KeyCombo>,
+        key: Box<KeyCombo>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub enum Statement {
+    Set { var: String, value: String },
+    Do { action: Action, on: KeyCombo },
+}
+
+#[derive(Debug, Clone)]
+pub struct Lexer {
+    input: String,
+    pos: usize,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Token {
+    Word(String),
+    Number(usize),
+    Hyphen,
+    OpenCurly,
+    CloseCurly,
+}
+
+impl Lexer {
+    pub fn new(s: String) -> Self {
+        Self { input: s, pos: 0 }
+    }
+
+    pub fn run(mut self) -> Vec<Token> {
+        let mut ts: Vec<Token> = vec![];
+        while !self.done() {
+            match self.peek().unwrap() {
+                c if c.is_whitespace() => {
+                    self.eat();
+                }
+                c if c.is_ascii_alphabetic() => {
+                    let begin = self.pos;
+                    while !self.done() && self.peek().unwrap().is_ascii_alphabetic() {
+                        self.eat();
+                    }
+                    let end = self.pos;
+                    ts.push(Token::Word(self.input[begin..end].to_string()));
+                }
+                c if c.is_ascii_digit() => {
+                    let begin = self.pos;
+                    while !self.done() && self.peek().unwrap().is_ascii_digit() {
+                        self.eat();
+                    }
+                    let end = self.pos;
+                    ts.push(Token::Number(self.input[begin..end].parse().unwrap()));
+                }
+                '-' => {
+                    self.eat();
+                    ts.push(Token::Hyphen);
+                }
+                '{' => {
+                    self.eat();
+                    ts.push(Token::OpenCurly);
+                }
+                '}' => {
+                    self.eat();
+                    ts.push(Token::CloseCurly);
+                }
+                '*' => {
+                    while !self.done() && self.peek().unwrap() != '\n' {
+                        self.eat();
+                    }
+                    self.eat();
+                }
+                _ => todo!(),
+            }
+        }
+        ts
+    }
+
+    fn done(&self) -> bool {
+        self.input.len() <= self.pos
+    }
+
+    fn peek(&self) -> Option<char> {
+        self.input.chars().nth(self.pos)
+    }
+
+    fn eat(&mut self) -> Option<char> {
+        self.pos += 1;
+        self.input.chars().nth(self.pos - 1)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use log::{error, warn, debug, trace, info};
+mod config;
+use log::{debug, error, info, trace, warn};
 
 use x11::{xinerama, xlib};
-
 
 struct Nwm {
     display: *mut xlib::Display,
@@ -12,7 +12,7 @@ struct Nwm {
     running: bool,
     last_x: i32,
     last_y: i32,
-    conf: Config
+    conf: Config,
 }
 
 const MOD_SHIFT: u32 = xlib::ShiftMask;
@@ -24,13 +24,13 @@ struct Rect {
     h: i32,
 }
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Copy)]
 enum MasterKey {
     Super,
     Alt,
-    Shift
+    Shift,
 }
 
 impl Into<u32> for MasterKey {
@@ -43,13 +43,11 @@ impl Into<u32> for MasterKey {
     }
 }
 
-
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 struct Config {
     master_key: MasterKey,
 }
-
 
 impl Config {
     pub fn get_master_key(&self) -> u32 {
@@ -60,19 +58,17 @@ impl Config {
 impl std::default::Default for Config {
     fn default() -> Self {
         Self {
-            master_key: MasterKey::Super
+            master_key: MasterKey::Super,
         }
     }
 }
-
 
 pub type WindowId = u64;
 
 impl Nwm {
     pub fn create(display_name: &str) -> Option<Self> {
         let s = std::ffi::CString::new(display_name).unwrap();
-        let display: *mut xlib::Display =
-            unsafe { xlib::XOpenDisplay(s.as_ptr()) };
+        let display: *mut xlib::Display = unsafe { xlib::XOpenDisplay(s.as_ptr()) };
 
         if display.is_null() {
             error!("Display {} is not found", display_name);
@@ -85,11 +81,12 @@ impl Nwm {
             xlib::XSelectInput(
                 display,
                 xlib::XDefaultRootWindow(display),
-                xlib::SubstructureRedirectMask | xlib::SubstructureNotifyMask | xlib::KeyPressMask | xlib::PointerMotionMask,
+                xlib::SubstructureRedirectMask
+                    | xlib::SubstructureNotifyMask
+                    | xlib::KeyPressMask
+                    | xlib::PointerMotionMask,
             );
         };
-
-
 
         let mut conf = Config::default();
 
@@ -111,23 +108,19 @@ impl Nwm {
             std::process::Command::new("sh").arg(run_dir).spawn().ok();
         }
 
-
-
         info!("Everything went well in initialization :DD");
 
-        Some(
-            Self {
-                display,
-                workspaces: Default::default(),
-                curr_workspace: 0,
-                focused: Default::default(),
-                gap: 8,
-                running: true,
-                conf,
-                last_x: 0,
-                last_y: 0
-            }
-        )
+        Some(Self {
+            display,
+            workspaces: Default::default(),
+            curr_workspace: 0,
+            focused: Default::default(),
+            gap: 8,
+            running: true,
+            conf,
+            last_x: 0,
+            last_y: 0,
+        })
     }
 
     fn focused(&self) -> Option<usize> {
@@ -146,12 +139,7 @@ impl Nwm {
             let w = self.workspaces[self.curr_workspace][i];
             unsafe {
                 xlib::XRaiseWindow(self.display, w);
-                xlib::XSetInputFocus(
-                    self.display,
-                    w,
-                    xlib::RevertToParent,
-                    xlib::CurrentTime,
-                );
+                xlib::XSetInputFocus(self.display, w, xlib::RevertToParent, xlib::CurrentTime);
             }
             info!("Applied focus to window {w}");
         }
@@ -159,7 +147,15 @@ impl Nwm {
 
     fn grab_key(&mut self, key_code: u32, modifiers: u32) {
         unsafe {
-            xlib::XGrabKey(self.display, key_code as i32, modifiers, xlib::XDefaultRootWindow(self.display), 1, xlib::GrabModeAsync, xlib::GrabModeAsync);
+            xlib::XGrabKey(
+                self.display,
+                key_code as i32,
+                modifiers,
+                xlib::XDefaultRootWindow(self.display),
+                1,
+                xlib::GrabModeAsync,
+                xlib::GrabModeAsync,
+            );
         }
     }
 
@@ -200,15 +196,31 @@ impl Nwm {
             return;
         }
         unsafe {
-            let wm_prot = xlib::XInternAtom(self.display, b"WM_PROTOCOLS\0".as_ptr() as *const i8, xlib::False);
+            let wm_prot = xlib::XInternAtom(
+                self.display,
+                b"WM_PROTOCOLS\0".as_ptr() as *const i8,
+                xlib::False,
+            );
 
-            let wm_del = xlib::XInternAtom(self.display, b"WM_DELETE_WINDOW\0".as_ptr() as *const i8, xlib::False);
+            let wm_del = xlib::XInternAtom(
+                self.display,
+                b"WM_DELETE_WINDOW\0".as_ptr() as *const i8,
+                xlib::False,
+            );
 
             let mut prots: *mut xlib::Atom = std::ptr::null_mut();
             let mut count: i32 = 0;
 
-            if xlib::XGetWMProtocols(self.display, self.curr_ws()[window_index], &mut prots, &mut count) != 0 {
-                let supported = std::slice::from_raw_parts(prots, count as usize).iter().any(|&x| x == wm_del);
+            if xlib::XGetWMProtocols(
+                self.display,
+                self.curr_ws()[window_index],
+                &mut prots,
+                &mut count,
+            ) != 0
+            {
+                let supported = std::slice::from_raw_parts(prots, count as usize)
+                    .iter()
+                    .any(|&x| x == wm_del);
                 xlib::XFree(prots as *mut _);
 
                 if supported {
@@ -218,7 +230,10 @@ impl Nwm {
                     event.client_message.message_type = wm_prot;
                     event.client_message.format = 32;
                     event.client_message.data.set_long(0, wm_del as i64);
-                    event.client_message.data.set_long(1, xlib::CurrentTime as i64);
+                    event
+                        .client_message
+                        .data
+                        .set_long(1, xlib::CurrentTime as i64);
 
                     xlib::XSendEvent(
                         self.display,
@@ -235,7 +250,6 @@ impl Nwm {
             xlib::XKillClient(self.display, self.curr_ws()[window_index]);
             self.curr_ws_mut().remove(window_index);
         }
-
     }
 
     fn curr_ws_mut(&mut self) -> &mut Vec<WindowId> {
@@ -253,7 +267,7 @@ impl Nwm {
 
     pub fn run(mut self) {
         use std::mem::zeroed;
-        let mut event: xlib::XEvent = unsafe {zeroed()};
+        let mut event: xlib::XEvent = unsafe { zeroed() };
         let enter_code = self.keysym_to_keycode(x11::keysym::XK_Return);
         let h_code = self.keysym_to_keycode(x11::keysym::XK_H);
         let l_code = self.keysym_to_keycode(x11::keysym::XK_L);
@@ -300,50 +314,55 @@ impl Nwm {
 
         info!("Keybindings were setup");
 
-
         while self.running {
-            unsafe { xlib::XNextEvent(self.display, &mut event); }
+            unsafe {
+                xlib::XNextEvent(self.display, &mut event);
+            }
 
             match event.get_type() {
-                xlib::MapRequest => self.add_window(unsafe {event.map_request}),
-                xlib::UnmapNotify => self.remove_window(unsafe {event.unmap}),
+                xlib::MapRequest => self.add_window(unsafe { event.map_request }),
+                xlib::UnmapNotify => self.remove_window(unsafe { event.unmap }),
                 xlib::KeyPress => {
                     let key_event = unsafe { event.key };
-                    if key_event.state & self.conf.get_master_key() != 0 && key_event.state & MOD_SHIFT == 0 {
+                    if key_event.state & self.conf.get_master_key() != 0
+                        && key_event.state & MOD_SHIFT == 0
+                    {
                         match key_event.keycode {
                             x if x == enter_code => {
-                                std::process::Command::new("alacritty")
-                                    .spawn().unwrap();
-                            },
+                                std::process::Command::new("alacritty").spawn().unwrap();
+                            }
                             x if x == space_code => {
-                                std::process::Command::new("dmenu_run")
-                                    .spawn().unwrap();
+                                std::process::Command::new("dmenu_run").spawn().unwrap();
                             }
                             x if x == h_code => {
                                 self.focus_left();
-                            },
+                            }
                             x if x == l_code => {
                                 self.focus_right();
-                            },
+                            }
                             x if x == w_code => {
                                 if let Some(w) = self.focused() {
                                     self.close_window(w);
                                     self.swap_left();
                                 }
-                            },
+                            }
                             x if number_codes.contains(&x) => {
-                                self.switch_ws((x - self.keysym_to_keycode(x11::keysym::XK_1)) as usize);
+                                self.switch_ws(
+                                    (x - self.keysym_to_keycode(x11::keysym::XK_1)) as usize,
+                                );
                             }
                             _ => {}
                         }
-                    } else if key_event.state & self.conf.get_master_key() != 0 && key_event.state & MOD_SHIFT != 0 {
+                    } else if key_event.state & self.conf.get_master_key() != 0
+                        && key_event.state & MOD_SHIFT != 0
+                    {
                         match key_event.keycode {
                             x if x == h_code => {
                                 self.swap_left();
-                            },
+                            }
                             x if x == l_code => {
                                 self.swap_right();
-                            },
+                            }
                             x if x == q_code => {
                                 info!("Exiting nwm, byee!");
                                 self.running = false;
@@ -352,9 +371,9 @@ impl Nwm {
                         }
                     }
                 }
-                xlib::KeyRelease => {},
+                xlib::KeyRelease => {}
                 xlib::MotionNotify => {
-                    let motion_event = unsafe {event.motion};
+                    let motion_event = unsafe { event.motion };
                     if self.last_x != motion_event.x_root && self.last_y != motion_event.y_root {
                         let rects = self.window_rects();
                         for (i, r) in rects.iter().enumerate() {
@@ -368,9 +387,12 @@ impl Nwm {
                     }
                 }
                 xlib::MappingNotify => {
-                    self.refresh_mappings(unsafe {event.mapping});
+                    self.refresh_mappings(unsafe { event.mapping });
                 }
-                xlib::CreateNotify | xlib::MapNotify | xlib::DestroyNotify | xlib::ConfigureNotify => {}
+                xlib::CreateNotify
+                | xlib::MapNotify
+                | xlib::DestroyNotify
+                | xlib::ConfigureNotify => {}
                 xlib::ConfigureRequest => self.layout(),
                 _ => {
                     warn!("Unknown event: {:#?}", event);
@@ -389,7 +411,6 @@ impl Nwm {
                 self.apply_focus();
             }
         }
-
     }
 
     fn switch_ws(&mut self, new_ws: usize) {
@@ -448,12 +469,23 @@ impl Nwm {
     }
 
     fn add_window(&mut self, event: xlib::XMapRequestEvent) {
-        unsafe {xlib::XMapWindow(self.display, event.window);}
+        unsafe {
+            xlib::XMapWindow(self.display, event.window);
+        }
         self.curr_ws_mut().push(event.window);
         self.focused[self.curr_workspace] = Some(self.curr_ws().len() - 1);
         self.layout();
-        unsafe {xlib::XMapRaised(self.display, event.window);}
-        unsafe {xlib::XSetInputFocus(self.display, event.window, xlib::RevertToPointerRoot, xlib::CurrentTime);}
+        unsafe {
+            xlib::XMapRaised(self.display, event.window);
+        }
+        unsafe {
+            xlib::XSetInputFocus(
+                self.display,
+                event.window,
+                xlib::RevertToPointerRoot,
+                xlib::CurrentTime,
+            );
+        }
     }
 
     fn map_window(&mut self, window_index: usize) {
@@ -506,7 +538,7 @@ impl Nwm {
         if let Some(i) = self.focused() {
             if i + 1 < self.curr_ws().len() {
                 self.curr_ws_mut().swap(i, i + 1);
-                self.focused[self.curr_workspace]= Some(i + 1);
+                self.focused[self.curr_workspace] = Some(i + 1);
                 self.layout();
                 self.apply_focus();
             }
@@ -514,11 +546,18 @@ impl Nwm {
     }
 
     fn focus_left(&mut self) {
-        self.focused[self.curr_workspace] = self.focused[self.curr_workspace].map(|x| if x == 0 {x} else {x - 1});
+        self.focused[self.curr_workspace] =
+            self.focused[self.curr_workspace].map(|x| if x == 0 { x } else { x - 1 });
         self.apply_focus();
     }
     fn focus_right(&mut self) {
-        self.focused[self.curr_workspace]= self.focused[self.curr_workspace].map(|x| if self.curr_ws().len() - 1 == x {x} else {x + 1});
+        self.focused[self.curr_workspace] = self.focused[self.curr_workspace].map(|x| {
+            if self.curr_ws().len() - 1 == x {
+                x
+            } else {
+                x + 1
+            }
+        });
         self.apply_focus();
     }
 
@@ -544,20 +583,25 @@ impl Nwm {
     }
     fn resize_window(&self, w: u64, width: u32, height: u32) {
         trace!("Resized window {w} to {width}x{height}");
-        unsafe {xlib::XResizeWindow(self.display, w, width, height);}
+        unsafe {
+            xlib::XResizeWindow(self.display, w, width, height);
+        }
     }
 
     fn keysym_to_keycode(&self, sym: u32) -> u32 {
-        unsafe {
-            xlib::XKeysymToKeycode(self.display, sym as u64) as u32
-        }
+        unsafe { xlib::XKeysymToKeycode(self.display, sym as u64) as u32 }
     }
 }
 
 impl Drop for Nwm {
     fn drop(&mut self) {
         unsafe {
-            xlib::XUngrabKey(self.display, xlib::AnyKey, xlib::AnyModifier, xlib::XDefaultRootWindow(self.display));
+            xlib::XUngrabKey(
+                self.display,
+                xlib::AnyKey,
+                xlib::AnyModifier,
+                xlib::XDefaultRootWindow(self.display),
+            );
             xlib::XUngrabPointer(self.display, xlib::CurrentTime);
             xlib::XCloseDisplay(self.display);
         }
@@ -566,7 +610,10 @@ impl Drop for Nwm {
 
 fn main() {
     env_logger::init();
+    let tokens =
+        dbg!(config::Lexer::new(std::fs::read_to_string("test_config.nwc").unwrap()).run());
+    dbg!(config::Parser::new(&tokens).parse());
+    return;
     let display_name = std::env::var("DISPLAY").unwrap();
     Nwm::create(&display_name).unwrap().run();
 }
-
