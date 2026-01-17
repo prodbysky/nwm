b334634a296da704af639735838a32a9b38a6d6c
issac <kyrbynyt@gmail.com>
2026-01-17 23:03:44 +0000

    impl shit ton of functions from better_x11

--- src/better_x11rb.rs
+++ src/better_x11rb.rs
@@ -1,8 +1,13 @@
-use x11rb::{connection::Connection, protocol::xproto::{ChangeWindowAttributesAux, ConnectionExt, EventMask, Screen}, rust_connection::RustConnection};
+use x11rb::{connection::Connection, protocol::{Event, xproto::{ChangeWindowAttributesAux, ConfigureWindowAux, ConnectionExt, EventMask, InputFocus, MappingNotifyEvent, Screen, StackMode, Time}}, rust_connection::RustConnection};
+
+use log::warn;
+
+pub type WindowId = u32;
 
 pub struct X11RB {
     conn: RustConnection,
-    screen: Screen
+    screen: Screen,
+    pointer_pos: (i16, i16)
 }
 
 impl X11RB {
@@ -25,6 +30,7 @@ impl X11RB {
         Self {
             screen: screen.clone(),
             conn,
+            pointer_pos: (0, 0)
         }
     }
 
@@ -40,5 +46,75 @@ impl X11RB {
             0_u32,
             x11rb::protocol::xproto::Time::CURRENT_TIME
         );
+    }
+
+    pub fn next_event(&mut self) -> Event {
+        let e = self.conn.wait_for_event().unwrap();
+
+        match e {
+            Event::MotionNotify(e) => {
+                self.pointer_pos.0 = e.root_x;
+                self.pointer_pos.1 = e.root_y;
+            }
+            Event::MappingNotify(e) => {
+                // NOTE: I can't actually understand this if stuff breaks look at THIS
+                self.update_mapping(e); 
+                self.conn.change_keyboard_mapping(e.count, e.first_keycode, 0, &[]);
+            }
+            ref e => {
+                warn!("Ignoring x11 event: {e:?}");
+            }
+        };
+
+        e
+    }
+
+    pub fn mouse_pos(&self) -> (i16, i16) {
+        self.pointer_pos
+    }
+
+    pub fn update_mapping(&mut self, e: MappingNotifyEvent) {
+        self.conn.change_keyboard_mapping(e.count, e.first_keycode, 0, &[]);
+    }
+
+    pub fn screen_size(&self) -> (u16, u16) {
+        (self.screen.width_in_pixels, self.screen.height_in_pixels)
+    }
+
+    pub fn raise_window(&mut self, id: WindowId) {
+        self.conn.map_window(id);
+        self.conn.configure_window(id, &ConfigureWindowAux::new().stack_mode(StackMode::ABOVE));
+        self.conn.flush();
+    }
+
+    pub fn map_window(&mut self, id: WindowId) {
+        self.conn.map_window(id);
+        self.conn.flush();
+    }
+
+    pub fn unmap_window(&mut self, id: WindowId) {
+        self.conn.unmap_window(id);
+        self.conn.flush();
+
+    }
+
+    pub fn focus_window(&mut self, id: WindowId) {
+        self.conn.set_input_focus(InputFocus::POINTER_ROOT, id, Time::CURRENT_TIME);
+        self.conn.flush();
+    }
+
+    pub fn close_window(&mut self, id: WindowId) {
+        self.conn.destroy_window(id);
+        self.conn.flush();
+    }
+
+    pub fn move_window(&mut self, id: WindowId, x: i16, y: i16) {
+        self.conn.configure_window(id, &ConfigureWindowAux::new().x(x as i32).y(y as i32));
+        self.conn.flush();
+    }
+
+    pub fn resize_window(&mut self, id: WindowId, w: u32, h: u32) {
+        self.conn.configure_window(id, &ConfigureWindowAux::new().width(w as u32).height(h as u32));
+        self.conn.flush();
     }
 }
